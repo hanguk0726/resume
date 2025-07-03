a40ff507402a94211ceda172c96fdb1e80aa018b
hanguk0726 <hanguk0726@gmail.com>
2025-07-03 18:22:06 +0900

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/home";
 import "./index.css";
 import About from "./routes/about";
 function App() {
   return (
-	<BrowserRouter>
-		<Routes>
-			<Route path="/resume" element={<Home />} />
-			<Route path="/resume/about" element={<About />} />
-			{/* 다른 경로가 있으면 여기에 추가 */}
-		</Routes>
-	</BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/resume" element={<Home />} />
+        <Route path="/resume/about" element={<About />} />
+        {/* 다른 경로가 있으면 여기에 추가 */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
